ace4782c7cda2755fc4a7277ac3f92b5f0af0f37
rachitsgambit <asthanarachit@gmail.com>
2023-07-01 18:45:15 +0530

    Update

--- repetitions.cpp
+++ repetitions.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 
 int main(){
-    int num;
+    int num , count =0;
+    char temp;
+    std::cout<<"Enter number"<<std::endl;
     std::cin>>num;
-    if(num>=1&&num<=(10^6)){
-
+    char* arr = new char[num];
+    bool* visited = new bool[num];
+    int* countArr = new int[num];
+    for(int i=0;i<num;i++){
+        visited[i] = false;
+        countArr[i] = 0;
     }
+    if(num>=1&& num<=(10 ^ 6)){
+        for(int i=0;i<num;i++)
+        {
+            std::cout<<"Enter element"<<std::endl;
+            std::cin>>arr[i];
+        }
+
+        // for(int i=0;i<num;i++){
+        //     for(int j=i+1;j<num;j++){
+        //         if(arr[i]>arr[j]){
+        //         temp = arr[i];
+        //         arr[i] = arr[j];
+        //         arr[j] = temp;
+        //     }
+        //     }
+        // }
+
+        for(int i=0; i<num;i++){
+            if(visited[i]!=true){
+                int count =1;
+                for(int j=i+1;j<num;j++){
+                    if(arr[i]==arr[j]){
+                        visited[j]=true;
+                        count++;
+                    }
+                }
+                visited[i] =true;
+                countArr[i] = count;
+            }
+        }
+        count =0;
+        for(int i=0;i<num;i++){
+            if(visited[i] = false){
+                count++;
+            }
+        }
+        std::cout<<"Unique elements are"<<count<<std::endl;
+
+        for(int i=0;i<num;i++){
+            if(visited[i]==1){
+                std::cout<<countArr[i]<<std::endl;
+            }
+        }
+    }
+    delete[] arr;
+    delete[] visited;
+    delete[] countArr;
     return 0;
 }
