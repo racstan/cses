a0d5f5c806936f2845f9fa5e37290bb307665a70
rachitsgambit <asthanarachit@gmail.com>
2023-07-11 22:56:37 +0530

    Final Upload

--- missingnumber.cpp
+++ missingnumber.cpp
@@ -1,62 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-class missingnum{
-    public:
-        int count;
-        int temp;
-    public:
-        missingnum(){
-            count =0;
-            temp =0;
-        }
-        missingnum(int count){
-            this->count = count;
-            this->temp = 0;
-        }
-
-        int input(){
-            std::cout<<"Enter the count of numbers"<<std::endl;
-            std::cin>>count;
-            return count;
-        }
-        int calculate(int count){
-            if(count!=0 && count>=2 && count<=(2*10^5)){
-               int* tempArr = new int(count);
-               for(int i=0;i<(count-1);i++){
-                std::cout<<"Enter element"<<std::endl;
-                std::cin>>temp;
-                tempArr[i]=temp;
-               }
-               for(int i=0;i<(count-1);i++){
-                for(int j=i+1;j<(count-1);j++){
-                    if(tempArr[i]>tempArr[j]){
-                        temp = tempArr[i];
-                        tempArr[i] = tempArr[j];
-                        tempArr[j] = temp;
-                    }
-                }
-               }
-                for(int i=0;i<count;i++){
-                    temp = tempArr[0]+i;
-                    if(temp!=tempArr[i]){
-                        std::cout<<"The missing number is "<<std::endl;
-                        std::cout<<temp<<std::endl;
-                        break;
-                    }
-                }
-               delete[] tempArr; 
-            }
-            else{
-                return -1;
-            }
-            return 0;
-        }
-
-};
+#define ll long long
 
 int main(){
-    missingnum i1;
-    int num = i1.input();
-    i1.calculate(num);
-    return 0;
+    ll n, s =0;
+    cin>>n;
+    for(int i =1; i<n;i++){
+        int a;
+        cin>>a;
+        s+=a;
+    }
+
+    cout<<n*(n+1)/2 - s;
 }
