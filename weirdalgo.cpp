ad3374b267940e973ccfdc3a1029bc601770075f
rachitsgambit <asthanarachit@gmail.com>
2023-07-11 22:49:18 +0530

    Final Upload

--- weirdalgo.cpp
+++ weirdalgo.cpp
@@ -3,16 +3,16 @@ using namespace std;
 #define ll long long
 
 int main(){
-    int n;
+    ll n;
     cin>>n;
+    cout<<n;
     while(n>1){
         if(n%2==0){
             n/=2;
-            cout<<" "<<n;
         }
         else{
-            n = n*3+1;
-            cout<<" "<<n;
+            n = 3*n+1;
         }
+        cout<<" "<<n;
     }
 }
