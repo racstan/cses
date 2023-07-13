9c39b1c67cdc70c9bceea10d46b9e26a67920d94
rachitsgambit <asthanarachit@gmail.com>
2023-07-13 19:26:37 +0530

    Final Upload

--- increasingArray.cpp
+++ increasingArray.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    int mx = 0;
+    ll mx = 0;
     ll ans = 0;
-    for(int i =0 ; i<n;i++) {
-        int s;
+    for(int i=0;i<n;i++){
+        long long s;
         cin>>s;
-        mx = max(x,mx);
-        ans += mx - x;
+        mx = max(mx,s);
+        ans+= mx - s;
     }
     cout<<ans;
 }
